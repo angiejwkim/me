99a7984645ec627c8f413774942f6c6624d6dc3d
angiejwkim <angiejwkim@gmail.com>
2025-01-07 08:36:50 -0500

    Fix Sanity environment variables for Vercel integration.

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,11 +11,6 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
 
-export const apiKey = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_STUDIO_API_TOKEN,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_API_TOKEN'
-);
-
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage);
